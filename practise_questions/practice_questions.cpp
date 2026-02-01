598799be01b6db98b7b8c5052e8e64867287598e
ahmedali839 <ahmedyarr1212@gmail.com>
2026-02-01 22:47:33 +0500

    practise questions

--- practise_questions/practice_questions.cpp
+++ practise_questions/practice_questions.cpp
@@ -1,11 +1,11 @@
-// lecture 02 __ Q-1 Write a C++ program that displays your name, registration number, and department.
 
+// lecture 02 __ Q-1 Write a C++ program that displays your name, registration number, and department.
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    cout << "Name: Ahmed." << endl;
+    cout << "Name: Ahmed" << endl;
     cout << "Registration Number: #25151219-999" << endl;
     cout << "Department: BSCS" << endl;
 
