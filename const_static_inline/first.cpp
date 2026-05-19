7b9ad2674143fd69035010328c3f3a9aa55e8fb1
Ahmed <ahmedyarr1212@gmail.com>
2026-05-19 19:09:47 +0500

    static, const with functions

---
+++ const_static_inline/first.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+// inline int square(int x)
+// {
+//     return x * x;
+// };
+
+void counter()
+{
+    static int count = 0;    // editable / modifiable as static
+    const int non_count = 0; // non-editable / non-modifiable as const
+
+    count++;
+    cout << non_count << endl;
+    cout << count << endl;
+}
+
+int main()
+{
+    // cout << square(5);
+
+    counter();
+    return 0;
+}
