06b598e6307181fe3878d9942a1173b0cd6c04be
Ahmed <ahmedyarr1212@gmail.com>
2026-05-20 16:19:19 +0500

    abstract_classes

---
+++ virtual/virtual.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class Shape
+{
+
+public:
+    // virtual void draw() = 0;
+};
+
+// class Circle : public Shape
+// {
+// public:
+//     void draw()
+//     {
+//         cout << "Drawing Circle";
+//     }
+// };
+
+int main()
+{
+    Shape s1;
+    // s1.draw();
+
+    // Circle c1;
+    // c1.draw();
+
+    return 0;
+}
