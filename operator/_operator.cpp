18110a0e47bfccf4248d5acef69c593b4a624e29
Ahmed <ahmedyarr1212@gmail.com>
2026-05-21 23:40:20 +0500

    operator_func built-in cpp

--- operator/_operator.cpp
+++ operator/_operator.cpp
@@ -1,25 +1,61 @@
+// #include <iostream>
+// using namespace std;
+
+// class Student
+// {
+// private:
+//     int x;
+
+// public:
+//     Student(int a) { x = a; }
+//     // void operator -() { x = -x; }
+
+//     void operator++()
+//     {
+//         ++x;
+//     }
+//     void display() { cout << x; }
+// };
+
+// int main()
+// {
+//     Student s1(4);
+//     s1.operator++();
+//     s1.display();
+// }
+
 #include <iostream>
 using namespace std;
 
-class Student
+class Complex
 {
 private:
-    int x;
+    int real, imag;
 
 public:
-    Student(int a) { x = a; }
-    // void operator -() { x = -x; }
-
-    void operator++()
+    Complex(int r = 0, int i = 0)
     {
-        ++x;
+        real = r;
+        imag = i;
     }
-    void display() { cout << x; }
+    Complex operator+(Complex c)
+    {
+        return Complex(real + c.real, imag + c.imag);
+    }
+
+    void display()
+    {
+        cout << real << " + " << imag << "i";
+    }
 };
 
 int main()
 {
-    Student s1(4);
-    s1.operator++();
-    s1.display();
+    Complex c1(2, 3), c2(4, 5);
+
+    Complex c3 = c1 + c2;
+
+    c3.display();
+
+    return 0;
 }
