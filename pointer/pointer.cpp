a866d3a44ec944ad41c6d65a3ee88d70299e0e5a
ahmedali839 <ahmedyarr1212@gmail.com>
2026-05-24 17:29:19 +0500

    pointers with string, func, int

--- pointer/pointer.cpp
+++ pointer/pointer.cpp
@@ -1,12 +1,36 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
-class Student
+class String
 {
+private:
+    char *str;
+
+public:
+    String(const char *s)
+    {
+        str = new char[strlen(s) + 1];
+        strcpy(str, s);
+    }
+
+    ~String()
+    {
+        delete[] str;
+    }
+    void display()
+    {
+        cout << str << endl;
+    }
 };
 
 int main()
 {
+
+    // objects
+    String strng("Ahmed Yar Hy");
+    strng.display();
+
     int t = 100;
     int *p = &t;
     *p = 20;
@@ -14,11 +38,12 @@ int main()
     int arr[5] = {10, 20, 30, 40, 50};
     int *arrPointer = arr;
 
-    int sum(){
-        cout << "Sum function called.";
-    }
+    // int sum()
+    // {
+    //     cout << "Sum function called.";
+    // }
 
-    int* sum();
+    // int *sum();
 
     cout << "t: " << t << endl;
     // cout << "&t: " << &t << endl;
@@ -27,20 +52,14 @@ int main()
     // cout << "&p: " << &p << endl;
     // cout << "new t: " << t << endl;
 
-
-
     // cout << "arr[0]: " << arr[0] << endl;
     // cout << "*arrPointer[0]: " << *(arrPointer + 0) << endl;
     // cout << "*arrPointer[1]: " << *(arrPointer + 1) << endl;
     // cout << "*arrPointer[2]: " << *(arrPointer + 2) << endl;
     // cout << "undefined: *arrPointer[99]: " << *(arrPointer + 99) << endl;
 
-
-    
     // for (int i = 0; i < 5; i++)
     // {
     //     cout << "*arrPointer[" << i << "]" << *(arrPointer + i) << endl;
     // }
-
-
 }
