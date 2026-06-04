1b4b33b77c89445c5d41592d0aad4e5b9f9d010a
ahmedali839 <ahmedyarr1212@gmail.com>
2026-06-04 23:28:51 +0500

    friends_func, static, this

--- virtual_examples/first.cpp
+++ virtual_examples/first.cpp
@@ -93,7 +93,7 @@ public:
     {
         cout << "Child1 Show" << endl;
     }
-    ~Child1(AbstractClass x) // desconstructor
+    ~Child1(AbstractClass x) // desconstructor function of class
     {
         cout << "Child1 Desconstructed, " << x.PrivateX << endl;
     }
