ed17e19e84a226fb6d07451534acef877616b36a
ahmedali839 <ahmedyarr1212@gmail.com>
2026-06-06 22:41:58 +0500

    friends_func, static, this

--- virtual_examples/first.cpp
+++ virtual_examples/first.cpp
@@ -95,7 +95,7 @@ public:
     }
     ~Child1(AbstractClass x)
     {
-        cout << "Child1 Desconstructed, " << x.PrivateX << endl;
+        cout << "Child1 Desconstructed, " << x.PrivateX << endl; // virtual
     }
 };
 class Child2 : virtual public AbstractClass
