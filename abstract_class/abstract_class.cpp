6560a5ebcf2b460686894d25afbe67facbc2fa00
Ahmed <ahmedyarr1212@gmail.com>
2026-06-10 06:01:40 +0500

    abstract_class

--- abstract_class/abstract_class.cpp
+++ abstract_class/abstract_class.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 using namespace std;
 
+class University {
+    virtual void username() = 0;
+};
+
+class Student: public University {
+    void username() {
+        cout << "Here's the student" << endl;
+    }
+}
+
 int main () {
-    
+
 }
