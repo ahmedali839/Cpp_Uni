ce6db04d88621827f5325869c5d0d06d21e9d471
Ahmed <ahmedyarr1212@gmail.com>
2026-06-11 08:25:39 +0500

    abstract_class

--- abstract_class/abstract_class.cpp
+++ abstract_class/abstract_class.cpp
@@ -3,14 +3,16 @@ using namespace std;
 
 class University
 {
+public:
     virtual void username() = 0;
 };
 
 class Student : public University
 {
+public:
     void username()
     {
-        cout << "Here's the student" << endl;
+        cout << "Here's the student" << endl; // it's abstract class
     }
 };
 
