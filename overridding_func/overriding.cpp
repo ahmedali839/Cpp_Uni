5a037f4003fd8273f0b9cf5b369b3110f9d20a9a
Ahmed <ahmedyarr1212@gmail.com>
2026-06-16 12:46:24 +0500

    overriding in cpp

--- overridding_func/overriding.cpp
+++ overridding_func/overriding.cpp
@@ -3,9 +3,21 @@ using namespace std;
 
 class University {
     public:
+    virtual void name(){
+        cout << "Unversity name"
+    }
+};
 
+class Student : public University {
+    public:
+    virtual void name(){
+        cout << "Student name"
+    }
 };
 
 int main () {
+
+    Student s1;
+    s1.name();
 
 }
