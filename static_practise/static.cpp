ff27d6fdf6cc8137ee58f73973dbe226640a7add
Ahmed <ahmedyarr1212@gmail.com>
2026-06-17 12:45:18 +0500

    static variables (remember value during func calls

---
+++ static_practise/static.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class StaticPractise
+{
+
+public:
+    static int count;
+    StaticPractise()
+    {
+        count++;
+    }
+    void ShowCount()
+    {
+        cout << "Count (updated): " << count << endl;
+    }
+};
+
+int StaticPractise::count = 1;
+
+int main()
+{
+    StaticPractise s1, s2, s3;
+    s3.ShowCount();
+
+    return 0;
+}
